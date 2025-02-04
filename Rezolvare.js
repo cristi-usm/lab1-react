2739596d8c0ae1912d576f93bd0d35fe433592ec
Crudu Cristian <cristian.crudu@usm.md>
2025-02-04 12:25:22 +0200

    Rezolvat

---
+++ Rezolvare.js
@@ -0,0 +1,11 @@
+var nr = 10; // number
+let text = "Hello"; // string
+const truthiness = true; // boolean
+
+function varType(variabila) {
+    return typeof variabila;
+}
+
+console.log(varType(nr)); // "number"
+console.log(varType(text)); // "string"
+console.log(varType(truthiness)); // "boolean"
