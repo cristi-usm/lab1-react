84ca242ec0c19602697351d9accdf3f044ff699b
Andriuta <janajana0416@gmail.com>
2025-02-12 11:56:58 +0200

    ex. 1-3 - 1.8

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
-// 1.2
+// 1.3
 
-function myFunction(x) {
-  if (typeof x === "number") {
-    console.log("x is a number");
-  }
-  if (typeof x === "string") {
-    console.log("x is a string");
+const integer = "123";
+const parsedInteger = parseInt(integer);
+console.log(parsedInteger);
+
+const str = 456;
+const strConverted = str.toString();
+console.log(strConverted);
+
+// 1.4
+
+const nume = "Arina";
+const varsta = 20;
+
+console.log(`Eu am o sora geamana care o cheama ${nume} si are ${varsta} ani.`);
+
+// 1.5
+let studenta = "Alexandra";
+
+studenta = "Luciana";
+
+console.log(studenta);
+
+// 1.6
+let z;
+
+function checkNullOrUndefined(z) {
+  if (z === null) {
+    console.log("Null");
+  } else if (z === undefined) {
+    console.log("Undefined");
   }
 }
 
-myFunction(12);
-myFunction("Hello");
+checkNullOrUndefined(z);
+
+// 1.7
+
+const obj = {
+  nume: "Daniela",
+  varsta: 20,
+  studenta: true,
+  adresa: {
+    strada: "Milano",
+    oras: "Italia",
+    blocul: 2,
+  },
+};
+
+obj.facultate = "Drept";
+obj.email = "dana@gmail.com";
+delete obj.adresa.blocul;
+
+console.log(obj);
+
+// 1.8
+
+const array = ["Mar", false, "Hello World!", 3.14, null, undefined];
+array.forEach((element) => console.log(typeof element));
