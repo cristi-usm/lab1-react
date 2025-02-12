125ac5582b35ae569f95355d93926d715cf50674
Bragaru Cristina <bragarucristina04@gmail.com>
2025-02-12 23:01:50 +0200

    Am uitat sa pun imaginile si .gitignore pentru Nastea

--- js/asyncs/js7_7.js
+++ js/asyncs/js7_7.js
@@ -16,45 +16,47 @@ function promise1(bool){
         });
     }
 }
-function promise2(bool){
-    if(bool === true){
-        return new Promise((resolve)=>{
-            setTimeout(()=>{
-                console.log("Promise solved ... I have no idea why it works, but it works.")
-                resolve("Promise solved ... I have no idea why it works, but it works.");
+function promise2(bool) {
+    if (bool === true) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log("promise 2")
+                resolve("promise 2");
             })
         });
     } else {
-        return new Promise((reject)=>{
-            setTimeout(()=>{
-                console.log("NOT WORKING. This is torturing me ... PLEASE HELP ");
-                reject(new Error("NOT WORKING. This is torturing me ... PLEASE HELP "));
-            })
-        });
-    }
-}
-function promise3(bool){
-    if(bool === true){
-        return new Promise((resolve)=>{
-            setTimeout(()=>{
-                console.log("Promise solved ... I have no idea why it works, but it works.")
-                resolve("Promise solved ... I have no idea why it works, but it works.");
-            })
-        });
-    } else {
-        return new Promise((reject)=>{
-            setTimeout(()=>{
-                console.log("NOT WORKING. This is torturing me ... PLEASE HELP ");
-                reject(new Error("NOT WORKING. This is torturing me ... PLEASE HELP "));
-            })
-        });
+        return new Promise((reject) => {
+            setTimeout(() => {
+                console.log("NOT WORKING. promise 2... PLEASE HELP ");
+                reject(new Error("NOT WORKING. promise 2"));
+            });
+        })
     }
 }
 
-console.log("Prea multe promisiuni, prea putine fapte....");
-promise1(true)
-    .then(promise2(false))
-    .then(promise3(true))
-    .catch((eroare) => {
-        console.error("A apărut o eroare:", eroare);
-    });
+function promise3(bool) {
+        if (bool === true) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    console.log("Promise solved ... promise 3.")
+                    resolve("Promise solved ...promise 3..");
+                })
+            });
+        } else {
+            return new Promise((reject) => {
+                setTimeout(() => {
+                    console.log("NOT WORKING.promise 3.... PLEASE HELP ");
+                    reject(new Error("NOT WORKING. Tpromise 3.... PLEASE HELP "));
+                })
+            });
+        }
+    }
+
+
+    console.log("Prea multe promisiuni, prea putine fapte....");
+    promise1(true)
+        .then(promise2(false))
+        .then(promise3(true))
+        .catch((eroare) => {
+            console.error("A apărut o eroare:", eroare);
+        });
