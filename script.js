0bec6a5ba35bb84284599869a3b85788a7a495cb
Denis.S <denissoltan700@gmail.com>
2025-02-14 17:59:25 +0200

    Exercitiul 4 - Rezolvat

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//1.1
+// 1.1
 var numarVar = 100;
 var textVar = "Hello!";
 var booleanVar = false;
@@ -15,7 +15,7 @@ console.log(numarVar, textVar, booleanVar);
 console.log(numarLet, textLet, booleanLet);
 console.log(numarConst, textConst, booleanConst);
 
-//1.2
+// 1.2
 function tipDeDate(variable) {
     return typeof variable;
 }
@@ -28,21 +28,21 @@ console.log(tipDeDate(numar));
 console.log(tipDeDate(text));
 console.log(tipDeDate(boolean));
 
-//1.3
+// 1.3
 let stringToNumber = Number("123");
 console.log(stringToNumber);
 
 let numberToString = String(456);
 console.log(numberToString);
 
-//1.4
+// 1.4
 let nume = "Denis";
 let varsta = 20;
 
 let propozitie = `Numele meu este ${nume} și am ${varsta} ani.`;
 console.log(propozitie);
 
-//1.5
+// 1.5
 let a = 5;
 let b = 10;
 
@@ -52,7 +52,7 @@ b = temp;
 
 console.log(a, b);
 
-//1.6
+// 1.6
 function verificare(val) {
     return val == null;
 }
@@ -60,42 +60,42 @@ function verificare(val) {
 console.log(verificare(null));      
 console.log(verificare(undefined));
 
-//1.7
-let obiect = { nume:"Denis", varsta: 27, student: true};
+// 1.7
+let obiect = { nume:"Denis", varsta: 20, student: true};
 delete obiect.student;
 console.log(obiect);
 
-//1.8
+// 1.8
 let array = [42, "text", true, {nume:"Denis"}, [1, 2, 3] ];
 array.forEach(el => console.log(typeof el));
 
 
-//2.1
+// 2.1
 function suma(a,b) {
     return a + b;
 }
 console.log(suma(3, 7));
 
-//2.2
+// 2.2
 function ariaDreptunghiului(latime, inaltime = 1) {
     return latime * inaltime;
 }
 console.log(ariaDreptunghiului(20));
 
-//2.3
+// 2.3
 const patrat = function(numar) {
     return numar * numar;
 }
 console.log(patrat(5));
 
-//2.4
+// 2.4
 function upperCase(text) {
     let upperText = text.toUpperCase();
     return [upperText, upperText.length];
 }
 console.log(upperCase("Salut"));
 
-//2.5
+// 2.5
 function dublu(numar) {
     return numar * 2;
 }
@@ -107,43 +107,138 @@ function calcule(numar) {
 }
 console.log(calcule(1));
 
-//2.6
+// 2.6
 const sageata = (a, b) => a + b;
 console.log(sageata(1, 2));
 
-//2.7
+// 2.7
 function altaFunctie(array, functie) {
     return array.map(functie);
 }
 console.log(altaFunctie([1, 2, 3], dublu));
 
-//3.1
+// 3.1
 let masina = { marca: "Toyota", model: "Avalon", an: 2013 };
 
-//3.2
+// 3.2
 console.log(masina.marca, masina["model"]);
 
-//3.3
+// 3.3
 masina.culoare = "rosu";
-masina.an = 2022;
+masina.an = 2014;
 
-//3.4
+// 3.4
 delete masina.model;
 
-//3.5
+// 3.5
+for (let cheie in masina) {
+    console.log(`${cheie}: ${masina[cheie]}`);
+}
 
-//3.6
+// 3.6
+masina.descriere = function() {
+    return `${this.marca}, ${this.culoare}, ${this.an}`;
+};
+console.log(masina.descriere());
 
-//3.7
+// 3.7
+function afiseazaPersoana(persoana) {
+    return `Nume: ${persoana.nume}, Varsta: ${persoana.varsta}`;
+}
+console.log(afiseazaPersoana({ nume: "Denis", varsta: 20 }));
 
-//3.8
+// 3.8
+console.log("culoare" in masina);
 
-//3.9
+// 3.9
+let casa = { adresa: { strada: "Mihai Eminescu", oras: "Chisinau" } };
+console.log(casa.adresa.strada);
 
-//3.10
+// 3.10
+function creeazaStudent(nume, varsta, nota) {
+    return { nume, varsta, nota };
+}
+console.log(creeazaStudent("Denis", 20, 9));
 
-//3.11
+// 3.11
+let masinaClone = Object.assign({}, masina);
+let masinaClone2 = { ...masina };
+console.log(masinaClone, masinaClone2);
 
-//3.12
+// 3.12
+let calculator = {
+    adunare: (a, b) => a + b,
+    scadere: (a, b) => a - b,
+    inmultire: (a, b) => a * b,
+    impartire: (a, b) => a / b
+};
+console.log(calculator.adunare(3, 5));
 
-//3.13
+// 3.13
+let { marca, culoare } = masina;
+console.log(marca, culoare);
+
+// 4.1
+let orase = ["Chisinau", "Bucuresti", "Iasi"];
+console.log(orase[0], orase[orase.length - 1]);
+
+// 4.2
+orase.push("Cluj");
+orase.unshift("Timisoara");
+orase.pop();
+console.log(orase);
+
+// 4.3
+let culori = ["rosu", "verde", "albastru"];
+for (let i = 0; i < culori.length; i++) {
+    console.log(culori[i]);
+}
+
+// 4.4
+let animale = ["caine", "pisica", "cal"];
+animale.forEach(animal => console.log(`Animal: ${animal}`));
+
+// 4.5
+function filtreazaNumere(array) {
+    return array.filter(num => num > 10);
+}
+console.log(filtreazaNumere([5, 12, 18, 3]));
+
+// 4.6
+let numere = [42, 15, 8, 23];
+numere.sort((a, b) => a - b);
+console.log(numere);
+
+// 4.7
+let patrate = numere.map(num => num * num);
+console.log(patrate);
+
+// 4.8
+let sumaTotal = numere.reduce((acc, num) => acc + num, 0);
+console.log(sumaTotal);
+
+// 4.9
+let animaleArray = ["caine", "pisica", "cal"];
+console.log(animaleArray.includes("pisica"));
+console.log(animaleArray.indexOf("cal"));
+
+// 4.10
+let array1 = ["mere", "pere"];
+let array2 = ["banane", "struguri"];
+let arrayCombinat = [...array1, ...array2];
+console.log(arrayCombinat);
+
+// 4.11
+let matrice = [[1, 2], [3, 4]];
+console.log(matrice[0][1]);
+
+// 4.12
+function eliminaDuplicate(array) {
+    return [...new Set(array)];
+}
+console.log(eliminaDuplicate([1, 2, 2, 3, 4, 4, 5]));
+
+// 4.13
+let subArray = numere.slice(1, 3);
+numere.splice(2, 1);
+console.log(subArray, numere);
